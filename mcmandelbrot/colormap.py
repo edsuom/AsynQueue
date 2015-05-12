fe4719f86dfcd1074a186403061a97aae088b8d0
edsuom <edsuom@a3b7badd-e2cd-4207-ba89-9dbf7d88ca34>
2015-05-12 20:17:49 +0000

    Have a choice of colormaps; just before changing headers to Apache license
    git-svn-id: svn+ssh://tellectual.com/svn/devel@573 a3b7badd-e2cd-4207-ba89-9dbf7d88ca34

--- mcmandelbrot/colormap.py
+++ mcmandelbrot/colormap.py
@@ -29,6 +29,7 @@ Scientific Visualization",
 U{http://www.sandia.gov/~kmorel/documents/ColorMaps/}.
 """
 
+import os.path
 from pkg_resources import resource_stream
 from array import array
 
@@ -44,13 +45,59 @@ class ColorMapper(object):
       each of many linearly increasing values to be mapped, in CSV
       format.
     """
-    fileName = "colormap.csv"
-    def __init__(self):
-        fh = resource_stream(__name__, self.fileName)
-        self.rgb = np.loadtxt(fh, delimiter=',', dtype=np.uint8)
-        fh.close()
+    N_blackRed = 2000
+    useBlackRed = True
+    fileName = "moreland.csv"
+
+    def __init__(self, useBlackRed=False):
+        if not useBlackRed:
+            useBlackRed = self.useBlackRed
+        if useBlackRed:
+            self.rgb = self.blackRedMap(self.N_blackRed)
+        else:
+            self.rgb = self.csvFileMap()
         self.jMax = len(self.rgb) - 1
 
+    def blackRedMap(self, N):
+        """
+        Returns an RGB colormap of dimensions C{Nx3} that transitions from
+        black to red, then red to orange, then orange to white.
+        """
+        ranges = [
+            [0.000, 1.0/3],  # Red component ranges
+            [0.8/3, 2.0/3],  # Green component ranges
+            [1.8/3, 1.000],  # Blue component ranges
+        ]
+        return self._rangeMap(N, ranges)
+
+    def _rangeMap(self, N, ranges):
+        rgb = np.zeros((N, 3), dtype=np.uint8)
+        kt = np.rint(N*np.array(ranges)).astype(int)
+        # Range #1: Increase red
+        rgb[0:kt[0,1],0] = np.linspace(0, 255, kt[0,1])
+        # Range #2: Max red, increase green
+        rgb[kt[0,1]:,0] = 255
+        rgb[kt[1,0]:kt[1,1],1] = np.linspace(0, 255, kt[1,1]-kt[1,0])
+        # Range #3: Max red and green, increase blue
+        rgb[kt[1,1]:,1] = 255
+        rgb[kt[2,0]:,2] = np.linspace(0, 255, kt[2,1]-kt[2,0])
+        return rgb
+        
+    def csvFile(self):
+        """
+        Returns an RGB colormap loaded from I{fileName} in my package
+        directory.
+        """
+        filePath = os.path.join(
+            os.path.dirname(__file__), self.fileName)
+        if os.path.exists(filePath):
+            fh = open(filePath)
+        else:
+            fh = resource_stream(__name__, self.fileName)
+        rgb = np.loadtxt(fh, delimiter=',', dtype=np.uint8)
+        fh.close()
+        return rgb
+    
     def __call__(self, x):
         result = array('B')
         np.rint(self.jMax * x, x)
