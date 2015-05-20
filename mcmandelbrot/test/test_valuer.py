a0903ede3a030efaf3e6057321fa1489473df7b7
edsuom <edsuom@a3b7badd-e2cd-4207-ba89-9dbf7d88ca34>
2015-05-20 22:22:54 +0000

    Adding unit tests for mcmandelbrot.valuer
    git-svn-id: svn+ssh://tellectual.com/svn/devel@613 a3b7badd-e2cd-4207-ba89-9dbf7d88ca34

--- mcmandelbrot/test/test_valuer.py
+++ mcmandelbrot/test/test_valuer.py
@@ -24,6 +24,10 @@
 Unit tests for mcmandelbrot.valuer
 """
 
+from contextlib import contextmanager
+from collections import namedtuple
+
+from scipy import weave
 import numpy as np
 
 from twisted.internet import defer
@@ -35,22 +39,61 @@ from testbase import TestCase
 class TestRunner(TestCase):
     verbose = True
 
+    class Yielded:
+        pass
+    msgProto = "With {:d} max iterations at {:d} points "+\
+               "from ({:+f}, {:+f}) to ({:+f}, {:+f}):\n{}\n"
+    
     def setUp(self):
         self.mv = valuer.MandelbrotValuer(1000, 3)
 
-    def crpmToXQD(self, cr, crpm, N):
+    @contextmanager
+    def _crpmToXQD(self, cr, ci, crpm, N):
         crMin = cr - crpm
         crMax = cr + crpm
-        qd = 0.25 * (crMax - crMin) / N
-        return np.linspace(crMin, crMax, N, dtype=np.float64), qd
+        Y = self.Yielded()
+        Y.x = np.linspace(crMin, crMax, N, dtype=np.float64)
+        Y.qd = 0.25 * (crMax - crMin) / N
+        yield Y
+        self.msg(
+            self.msgProto,
+            self.mv.N_values, N, crMin, ci, crMax, ci, Y.z)
+        
+    def _eval_point(self, N, x, ci):
+        code = """
+        #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+        int j;
+        for (j=0; j<Nx[0]; j++) {
+            Z1(j) = eval_point(j, kmax, X1(j), ci);
+        }
+        """
+        kmax = self.mv.N_values - 1
+        z = np.zeros(N, dtype=np.int)
+        weave.inline(
+            code, ['x', 'z', 'ci', 'kmax'],
+            customize=self.mv.infoObj, support_code=self.mv.support_code)
+        return z
+
+    def test_eval_point(self):
+        N    = 10
+        cr   = -0.5332179372197309
+        ci   = +0.535867264573991
+        crpm = 0.0022400000000000002
+        #--------------------------------------
+        with self._crpmToXQD(cr, ci, crpm, N) as Y:
+            Y.z = self._eval_point(N, Y.x, ci)
+        self.assertEqual(min(Y.z), 75)
+        self.assertEqual(max(Y.z), 999)
         
     def test_run_computeValues(self):
         N    = 10
         cr   = -0.5332179372197309
         ci   = +0.535867264573991
         crpm = 0.0022400000000000002
-       #--------------------------------------
-        x, qd = self.crpmToXQD(cr, crpm, N)
-        z = self.mv.computeValues(N, x, ci, qd)
-        print z
+        #--------------------------------------
+        with self._crpmToXQD(cr, ci, crpm, N) as Y:
+            Y.z = self.mv.computeValues(N, Y.x, ci, Y.qd)
+        self.assertEqual(min(Y.z), 232)
+        self.assertEqual(max(Y.z), 4995)
+
         
